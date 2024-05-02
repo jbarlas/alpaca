88ea69ad07da1653d94f450dedf5cd39ca10ef1f
jbarlas <justin.barlas.5975@gmail.com>
2024-05-02 00:41:04 -0400

    connect to client

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/jbarlas/alpaca
+
+go 1.22.2
+
+require (
+	github.com/alpacahq/alpaca-trade-api-go/v3 v3.4.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	cloud.google.com/go v0.112.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+)
--- main/main.go
+++ main/main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/alpacahq/alpaca-trade-api-go/v3/alpaca"
+	"github.com/alpacahq/alpaca-trade-api-go/v3/marketdata"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello Alpaca!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	_ = alpaca.NewClient(alpaca.ClientOpts{
+		APIKey:    os.Getenv("AlpacaAPIKey"),
+		APISecret: os.Getenv("AlpacaAPISecret"),
+		BaseURL:   "https://paper-api.alpaca.markets/",
+	})
+	marketClient := marketdata.NewClient(marketdata.ClientOpts{
+		APIKey:    os.Getenv("AlpacaAPIKey"),
+		APISecret: os.Getenv("AlpacaAPISecret"),
+	})
+
+	fiveMinDur, _ := time.ParseDuration("10m")
+
+	request := marketdata.GetCryptoBarsRequest{
+		TimeFrame: marketdata.OneMin,
+		Start:     time.Now().Add(-fiveMinDur),
+		End:       time.Now(),
+	}
+
+	bars, err := marketClient.GetCryptoBars("DOGE/USD", request)
+	if err != nil {
+		panic(err)
+	}
+	for _, bar := range bars {
+		fmt.Printf("%+v\n", bar)
+	}
 }
